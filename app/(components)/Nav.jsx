e00652de82423948756d83992180683d866d41c2
joshua girma debele <joshdebele8@gmail.com>
2024-01-09 21:50:33 -0700

    Navbar Links

--- app/(components)/Nav.jsx
+++ app/(components)/Nav.jsx
@@ -1,7 +1,20 @@
-const Nav = () => {
-  return (
-    <div>Nav</div>
-  )
-}
+import { faHome, faTicket } from "@fortawesome/free-solid-svg-icons";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import Link from "next/link";
 
-export default Nav
+const Nav = () => {
+  return <nav>
+    <div>
+        <Link href="/">
+            <FontAwesomeIcon icon={faHome} className="icon" />
+        </Link>
+        <Link href="/TicketPage/new">
+            <FontAwesomeIcon icon={faTicket} className="icon" />
+        </Link>
+    </div>
+    <div>
+        <p className="text-default-text">joshdebele8@gmail.com</p>
+    </div>
+  </nav>
+}
+export default Nav;
