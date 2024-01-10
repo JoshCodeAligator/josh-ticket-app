4c6b36c420d01f262f40bb91fd4325a357249657
joshua girma debele <joshdebele8@gmail.com>
2024-01-10 12:13:09 -0700

    Added Styling in Global CSS

--- app/TicketPage/[id]/page.jsx
+++ app/TicketPage/[id]/page.jsx
@@ -1,7 +1,8 @@
-const TicketPage = ({params}) => {
-  return (
-    <div>TicketPage {params.id}</div>
-  )
+import TicketForm from "@/app/(components)/TicketForm"
+
+const TicketPage = ({ params }) => {
+  return <TicketForm/>
+  
 }
 
 export default TicketPage
