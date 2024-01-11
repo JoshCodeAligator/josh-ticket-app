95abada77c65c3dbb669fc898b3831ede7896d0f
joshua girma debele <joshdebele8@gmail.com>
2024-01-11 12:03:41 -0700

    TicketPage updates

--- app/TicketPage/[id]/page.jsx
+++ app/TicketPage/[id]/page.jsx
@@ -1,8 +1,23 @@
-import TicketForm from "@/app/(components)/TicketForm"
+import TicketForm from "@/app/(components)/TicketForm";
 
-const TicketPage = ({ params }) => {
-  return <TicketForm/>
-  
-}
+const getTicketById = async (id) => {
+  const res = await fetch(`http://localhost:3000/api/Tickets/${id}`, {
+    cache: "no-store",
+  });
 
-export default TicketPage
+  if (!res.ok) {
+    throw new Error("Failed to get ticket.")
+  }
+};
+
+const TicketPage = async ({ params }) => {
+  const EDITMODE = params.id === "new" ? false : true;
+  let updateTicketData = {};
+
+  if (EDITMODE) {
+    updateTicketData = await getTicketById(params.id);
+  }
+  return <TicketForm />;
+};
+
+export default TicketPage;
